8edc76f1eeb881076b933f2a82d34e43f433a767
Aleksandr-Pod <stdfire@gmail.com>
2022-04-20 11:34:27 +0300

    initial

---
+++ src/components/Section.styled.js
@@ -0,0 +1,11 @@
+import style from 'styled-components';
+
+export const SectionBox = style.div`
+margin: 10px;
+padding: 5px;
+width: 320px;
+background-color: yellow;
+border: 1px solid black;
+border-radius: 5px;
+text-align: center
+`
